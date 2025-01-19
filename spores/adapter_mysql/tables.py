7aba48ab47eeb9b70d15283fbbfcec39078ec7e5
0xkylecarter <194396695+0xkylecarter@users.noreply.github.com>
2025-01-19 21:35:53 +0800

    fix: initial table creation

--- spores/adapter_mysql/tables.py
+++ spores/adapter_mysql/tables.py
@@ -1,5 +1,5 @@
 mysql_tables = """
-CREATE TABLE `memories` (
+CREATE TABLE IF NOT EXISTS `memories` (
   `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `user_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
   `agent_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
